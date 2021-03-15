751bba807164a037602020589efcee00efbf74c2
AMROAMRO404 <amro.amro1999@gmail.com>
2021-03-15 18:01:31 +0200

    final code

--- Assignment 3/neural_network.py
+++ Assignment 3/neural_network.py
@@ -5,23 +5,17 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
 
 iris = load_iris()
 X = iris['data']
 Y = iris['target']
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4)
-
-# scaling the features
-scaler = StandardScaler()
-scaler.fit(X_train)
-
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
 
 number_of_neurons = 5
-mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(number_of_neurons),
-                    max_iter=3000)
+mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(
+    number_of_neurons), random_state=42, max_iter=6000)
 mlp.fit(X_train, Y_train)
 nn_pridictions = mlp.predict(X_test)
 print(
@@ -31,9 +25,9 @@ print(
 
 # study the overfit as we increase the hidden neurons
 to_study_accurecy = []
-for i in range(10):
-    mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(i+1),
-                        max_iter=3000)
+for i in range(15):
+    mlp = MLPClassifier(
+        solver='lbfgs', hidden_layer_sizes=(i+1), random_state=42, max_iter=6000)
     mlp.fit(X_train, Y_train)
     to_study_accurecy.append(mlp.score(X_test, Y_test))
 
@@ -42,8 +36,8 @@ print("3 fold cross validation : ")
 scores = cross_val_score(mlp, X, Y, cv=3)
 print(scores)
 
-xpoints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-plt.plot(xpoints, to_study_accurecy, marker='o')
+#xpoints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+plt.plot(to_study_accurecy, marker='o')
 plt.xlabel('number of hidden neurons')
 plt.ylabel('accuracy')
 plt.show()
